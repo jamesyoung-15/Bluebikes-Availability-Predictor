6163a38e7345b7faa390b16cea9b23c5e8d80c66
James Young <jyyoung@jyymail.com>
2026-03-05 12:43:17 -0500

    pin terraform cloudlfare version to prevent breaking changes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 4.29"
+      version = "4.44"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.4"
+      version = ">= 5.72"
     }
   }
 }
